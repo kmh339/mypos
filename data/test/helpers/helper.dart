977cd7906e6f0d2dcd69f5efa8d7e9d3a5fabba4
kmh339 <kmh339@gmail.com>
2022-08-23 00:14:40 +0900

    docs: refactor a readme

--- data/test/helpers/helper.dart
+++ data/test/helpers/helper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/services.dart';
-import 'package:flutter/widgets.dart';
 
 Future<String> readJson(String fileName) async {
   return await rootBundle.loadString('test/helpers/mock_data/$fileName');
