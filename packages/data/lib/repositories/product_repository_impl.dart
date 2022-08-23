fb42c4ba48c6010ba7c311de80221c3e87a312d8
kmh339 <kmh339@gmail.com>
2022-08-24 01:47:31 +0900

    bugfix: remove unuse json

--- packages/data/lib/repositories/product_repository_impl.dart
+++ packages/data/lib/repositories/product_repository_impl.dart
@@ -8,9 +8,29 @@ import 'package:domain/repositories/product_repository.dart';
 class ProductRepositoryImpl implements ProductRepository {
   @override
   Future<List<Product>> getProducts() async {
-    final Map<String, dynamic> response = jsonDecode(
-      await readJson('lib/helpers/mock_data/mock_products_response.json'),
-    );
+    // final Map<String, dynamic> response = jsonDecode(
+    //   await readJson('lib/helpers/mock_data/mock_products_response.json'),
+    // );
+
+    final Map<String, dynamic> response = {
+      "data": [
+        {
+          "id": 1,
+          "name": "마라탕",
+          "price": 19000.0
+        },
+        {
+          "id": 2,
+          "name": "탕수육",
+          "price": 14000.0
+        },
+        {
+          "id": 3,
+          "name": "짜장면",
+          "price": 3000.0
+        }
+      ]
+    };
 
     final List<dynamic> productsResponse = response['data'] as List<dynamic>;
 
